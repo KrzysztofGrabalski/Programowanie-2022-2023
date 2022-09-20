52545c0bc58f1beb98c530121010a224992eb270
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-20 09:54:29 +0200

    Napiszalem cos i wogule to pozdro pyrra yalla k1 machen

--- Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
+++ Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Powtorzenie.h"
 
 void showMenu()
 {
@@ -27,7 +28,27 @@ void doSelectedTask()
 	}
 }
 
-void mainProgram()
+void zadanie5()
+{
+	std::cout << "Podaj liczbe do zamiany na system binarny:\n";
+	int usernumber;
+	std::cin >> usernumber;
+	
+	std::string finalnumber;
+	while (usernumber)
+		wynik = (usernumber % 2) + finalnumber;
+	usernumber /:2
+}
+		
+	std::cout << "Podana wczeœniej przez ciebie liczba to:\n";
+	std::cout << usernumber;
+	std::cout << "Finalny efekt zamiany liczby dziesietnej na liczbe binarna wynosi:\n";
+	std::cout << finalnumber;
+	
+}
+
+
+
 {
 	//1. wyœwietlenie menu
 	showMenu();
