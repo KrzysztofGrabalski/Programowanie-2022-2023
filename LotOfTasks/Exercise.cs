8b422fb63b4146d3f11669735a65552ebd339b86
Mausek <112874771+KrzysztofGrabalski@users.noreply.github.com>
2023-01-17 09:41:13 +0100

    Add files via upload
    zadanka

---
+++ LotOfTasks/Exercise.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotOfTasks
+{
+    class Exercise
+    {
+        public void Exercise1()
+        {
+            int z = 23;
+            int x = 31;
+            var suma = (x + z) / 2;
+            Console.WriteLine("Wynik to jest" + suma);
+        }
+
+        public void Excercise2()
+        {
+            int a = 5;
+            int b = 9;
+            var pole = a * b;
+            Console.WriteLine("Pole prostokata to" + pole);
+        }
+
+        public void Excercise3()
+        {
+            int h = 8;
+            int r = 4;
+        }
+
+        public void Excercise5()
+        {
+            int a = 4;
+            int b = 8;
+
+            var potegi = a * a + b * b;
+            Console.WriteLine("Potega to aktualnie" + potegi);
+
+        }
+
+        public void zadanieif()
+        {
+            int liczba = 44;
+
+
+        }
+
+        public void zadFor1()
+        {
+            string num1 = Console.ReadLine();
+            int num = int.Parse(num1);
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + ",");
+            }
+
+        }
+
+        public void zadFor5()
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i);
+                for
+                {
+                     
+                }
+            }
+        }
+    }
+}
